8554419eb0cf5e6f15f287dce4905f6ae241b2c0
DazRave <hello@dazrave.uk>
2023-08-28 22:47:58 +0100

    admin area

---
+++ src/frontend/admin-users.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+// Check if user is logged in and is 'Darren'
+if (isset($_SESSION['username']) && $_SESSION['username'] === 'Darren') {
+    $db = new SQLite3('/var/www/html/main.db');
+    $result = $db->query('SELECT Username FROM Users');
+
+    $usernames = [];
+    while ($row = $result->fetchArray()) {
+        $usernames[] = $row['Username'];
+    }
+} else {
+    header("Location: index.php"); // Redirect if not logged in as 'Darren'
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Users</title>
+</head>
+<body>
+    <h1>Admin Users</h1>
+    <?php if (isset($usernames)): ?>
+        <ul>
+            <?php foreach ($usernames as $username): ?>
+                <li><?php echo $username; ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
+
+    <p><a href="index.php">Back to Home</a></p>
+</body>
+</html>
