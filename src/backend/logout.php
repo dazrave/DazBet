b77fcdf88d87d418ba297bd331ed5e22b2f3c0ec
DazRave <hello@dazrave.uk>
2023-08-28 22:40:24 +0100

    login logout

--- src/backend/logout.php
+++ src/backend/logout.php
@@ -5,6 +5,6 @@ session_start();
 session_destroy();
 
 // Redirect the user back to the login page or any other page you desire
-header("Location: login.html");
+header("Location: /index.php");
 exit();
 ?>
