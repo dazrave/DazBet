fd8ab6ef4518f0333044ee74c659724b57de1f4d
DazRave <hello@dazrave.uk>
2023-08-28 22:35:18 +0100

    login logout

--- src/frontend/index.php
+++ src/frontend/index.php
@@ -1,9 +1,27 @@
+<?php
+session_start();
+
+// Check if user is already logged in
+if (isset($_SESSION['username'])) {
+    $loggedIn = true;
+    $username = $_SESSION['username'];
+} else {
+    $loggedIn = false;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hello World PHP</title>
+    <title>Welcome to DazBet</title>
 </head>
 <body>
-    <a href="login.php">Login</a>
+    <?php if ($loggedIn): ?>
+        <p>Welcome, <?php echo $username; ?>!</p>
+        <a href="backend/login.php?logout">Logout</a>
+    <?php else: ?>
+        <p>Welcome to DazBet</p>
+        <a href="backend/login.html">Login</a>
+    <?php endif; ?>
 </body>
 </html>
