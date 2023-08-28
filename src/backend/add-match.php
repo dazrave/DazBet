21f8407ebf28818b1796c5779d45254ed6490672
DazRave <hello@dazrave.uk>
2023-08-28 23:52:06 +0100

    admin area

--- src/backend/add-match.php
+++ src/backend/add-match.php
@@ -9,11 +9,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $db = new SQLite3('/var/www/html/main.db');
 
     // Insert match into the database
-    $stmt = $db->prepare('INSERT INTO Matches (TeamA, TeamB, Timestamp) VALUES (:teamA, :teamB, datetime("now"))');
-    $stmt->bindValue(':teamA', implode(', ', $teamA), SQLITE3_TEXT);
-    $stmt->bindValue(':teamB', implode(', ', $teamB), SQLITE3_TEXT);
+    $stmt = $db->prepare('INSERT INTO Matches (Status, WinnerTeam) VALUES ("Ongoing", NULL)');
+    $result = $stmt->execute();
 
-    if ($stmt->execute()) {
+    if ($result) {
+        $matchID = $db->lastInsertRowID(); // Get the last inserted match ID
+
+        // Insert players into PlayersInMatches table
+        foreach ($teamA as $player) {
+            $stmt = $db->prepare('INSERT INTO PlayersInMatches (MatchID, UserID, Team) VALUES (:matchID, :userID, "Team A")');
+            $stmt->bindValue(':matchID', $matchID, SQLITE3_INTEGER);
+            $stmt->bindValue(':userID', $player, SQLITE3_INTEGER);
+            $stmt->execute();
+        }
+
+        foreach ($teamB as $player) {
+            $stmt = $db->prepare('INSERT INTO PlayersInMatches (MatchID, UserID, Team) VALUES (:matchID, :userID, "Team B")');
+            $stmt->bindValue(':matchID', $matchID, SQLITE3_INTEGER);
+            $stmt->bindValue(':userID', $player, SQLITE3_INTEGER);
+            $stmt->execute();
+        }
+
         echo "Match created successfully.";
     } else {
         echo "Error creating match.";
